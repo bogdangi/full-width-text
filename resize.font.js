2f1dbb1450c8eba3d5bb325e984167b66d41add8
Bogdan Girman <bogdan.girman@gmail.com>
2013-03-14 13:11:36 +0200

    Create resize.font.js

---
+++ resize.font.js
@@ -0,0 +1,10 @@
+function OneLineResizedText(el){
+  var stepSize = stepSize || 1;
+	var height = $(el).height();
+	var lineHeight = parseFloat($(el).css('line-height'));
+	while (height > lineHeight){
+		fontSize = parseFloat($(el).css('font-size'));
+		$(el).css('font-size', fontSize - stepSize + "px");
+		height = $(el).height();
+	}
+}
