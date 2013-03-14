bcf8b0a695d0b288813f93b0ce1d540ad4cd634b
Bogdan Girman <bogdan.girman@gmail.com>
2013-03-14 13:20:31 +0200

    Update resize.font.js

--- resize.font.js
+++ resize.font.js
@@ -1,10 +1,32 @@
-function OneLineResizedText(el){
-  	var stepSize = stepSize || 1;
-	var height = $(el).height();
-	var lineHeight = parseFloat($(el).css('line-height'));
+/*global jQuery */
+/*!
+* oneLineResizedText.js 1.0
+*
+* Copyright 2013, Bogdan Girman http://girman.in.ua
+* Released under the GPL license
+* http://www.gnu.org/licenses/gpl.html
+*
+* Date: Thu Mar 14 13:20:10 EET 2013
+*/
+(function( $ ){
+
+  $.fn.oneLineResizedText = function(stepSize){
+
+    // Setup options
+    var stepSize = stepSize || 1;
+
+    return this.each(function(){
+        el = $(this);
+	var height = el.height();
+	var lineHeight = parseFloat(el.css('line-height'));
 	while (height > lineHeight){
-		fontSize = parseFloat($(el).css('font-size'));
-		$(el).css('font-size', fontSize - stepSize + "px");
-		height = $(el).height();
+		fontSize = parseFloat(el.css('font-size'));
+		el.css('font-size', fontSize - stepSize + "px");
+		height = el.height();
 	}
-}
+      
+    });
+
+  };
+
+})( jQuery );
